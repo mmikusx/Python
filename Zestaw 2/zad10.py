5b1fb996927229f8093008f1832cf8004eccd2fc
Mikolaj <mikolajski007@gmail.com>
2022-10-09 16:12:37 +0200

    abcd

--- Zestaw 2/zad10.py
+++ Zestaw 2/zad10.py
@@ -1,1 +1,3 @@
-zdzxdxd
+# Mamy dany napis wielowierszowy line. Podać sposób obliczenia liczby wyrazów w napisie. Przez wyraz rozumiemy ciąg
+# "czarnych" znaków, oddzielony od innych wyrazów białymi znakami (spacja, tabulacja, newline).
+
