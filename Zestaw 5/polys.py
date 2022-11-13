c6b9d7b05aa8777f8cabeccc4e3608560d13bd36
Mikolaj <mikolajski007@gmail.com>
2022-11-13 22:31:52 +0100

    Zestaw 5 zrobiony

--- Zestaw 5/polys.py
+++ Zestaw 5/polys.py
@@ -106,19 +106,3 @@ def diff_poly(poly):               # pochodna wielomianu
         result.append(poly[i] * j)
 
     return result
-
-p1 = [-3, 0, 1, 2]
-p2 = [1, 0, -3, 11]
-p3 = add_poly(p1, p2)
-p4 = sub_poly(p1, p2)
-p5 = mul_poly(p1, p2)
-p6 = [0, 2, 1]
-p7 = [2, 1]
-print(p3, p4, p5, is_zero(p6), eq_poly(p6, p7), eval_poly(p2, 2), diff_poly([7, 0, 3, -1, 3]))
-
-# # 3x^2 + 2x - 1 = 3 * (2x - 1)^2 + 2 (2x - 1) - 1 = 3 * (4x^2 - 4x + 1) + 4x - 2 - 1 = 12x^2 - 12x + 3 +4x - 2 - 1 = 12x^2 - 8x
-# # x = 2x - 1
-#
-pp = [3, 2, -1]
-po = [2, -1]
-print(combine_poly(pp, po))
